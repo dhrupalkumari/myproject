561ed0c124ac858a197abcd11af906c4d576b2f8
dhrupalkumari <dhrupalpatel41@gmail.com>
2015-08-31 19:34:30 -0500

    My first comment
    My first comment

---
+++ HomeWorkIfStatement1.java
@@ -0,0 +1,33 @@
+package com.comcast.ifstatement;
+
+public class HomeWorkIfStatement1 {
+
+	public static void main(String[] args) {
+		ifElse();
+	}
+
+	public static void ifElse() {
+
+		int x = 10;
+		int y = 25;
+		if (x == y) {
+			System.out.println(" x == y");
+
+		}
+		if (x != y) {
+			System.out.println("x!=y ");
+
+		}
+
+		if (x >= y) {
+			System.out.println("x>=y");
+
+		}
+		if (x <= y) {
+			System.out.println("x <=y");
+
+		}
+
+	}
+
+}
